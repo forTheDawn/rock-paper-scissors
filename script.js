b104877599f40d2bf83ed47899bb037d1c47a320
Name <davidtjandra331@gmail.com>
2025-06-30 13:57:59 -0700

    Created a placeholder function that would get user input.

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+    let answer = prompt("Enter your answer here: ");
+
+    if (answer.toLowerCase() == "scissors") {
+        const humanAnswer = 
+
+    } else if (answer.toLowerCase() == "rock") {
+
+    } else if (answer.toLowerCase() == "paper") {
+
+    } else {
+
+    }
 
 }
 
@@ -20,4 +32,4 @@ function playRound(humanChoice, computerChoice) {
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
+playRound(humanSelection, computerSelection);

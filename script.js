2b03dfb4bd1ca331baa108a7a0fec6d2fdd75b5b
Name <davidtjandra331@gmail.com>
2025-07-02 09:09:38 -0700

    Added quotes.

--- script.js
+++ script.js
@@ -46,33 +46,33 @@ function playGame () {
         console.log("Scissors beats paper, human wins!")
         humanScore++;
 
-        } else if (humanChoice == scissors && computerChoice == rock) {
+        } else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("Rock beats scissors, computer wins!")
         computerScore++;
 
-        } else if (humanChoice == paper && computerChoice == rock) {
+        } else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("Paper beats rock, human wins!")
         humanScore++;
         
-        } else if (humanChoice == paper && computerChoice == scissors) {
+        } else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("Paper beats rock, computer wins!")
         computerScore++;
         
-        } else if (humanChoice == rock && computerChoice == scissors) {
+        } else if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("Paper beats rock, human wins!")
         humanScore++;
         
-        } else if (humanChoice == rock && computerChoice == paper) {
+        } else if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("Paper beats rock, computer wins!")
         computerScore++;
+        
         } else {
         console.log("It's a tie!")
         }
 }
-                for (var i = 0; i = 5; i++) playRound(humanChoice, computerChoice);
 
 }
 
 
 
-playGame();
+playGame(5);
